f12b22e9666849e68e6408fb041b8800cc69170a
BB-Code <15919854639@163.com>
2023-03-15 17:53:44 +0800

    feat: add project

---
+++ template/dialog.js
@@ -0,0 +1,84 @@
+const formGenerator = (obj) => {
+    let result = '';
+    obj.forEach(element => {
+        switch (element.name) {
+            case 'select':
+                result += `<el-form-item label="${element.label}" prop="${element.prop}">
+                <el-select v-model="dialogProps.rowData!.${element.model}" clearable></el-select>
+            </el-form-item>`
+                break;
+            case 'input':
+                result += `<el-form-item label="${element.label}" prop="${element.prop}">
+                <el-input v-model="dialogProps.rowData!.${element.model}" clearable></el-input>
+            </el-form-item>`
+                break;
+            default:
+                break;
+        }
+    });
+
+    return result
+}
+
+const ruleGenerator = (obj) => {
+    let result = '';
+    obj.forEach(element => {
+        result += `${element.prop}: [{ required: true, message: "请${element.name == 'select' ? '选择' : element.name == 'input' ? '输入' : ''}${element.label}", trigger: "blur" }],`
+    });
+    return result
+}
+
+const DialogGenerator = ({
+    name,
+    width,
+    type, // 1 表单 2 标签导航
+    itemArray, // [{name:'',label:'',prop:'',model:''}]
+}) => {
+    let template = `<template>
+	<div>
+		<el-dialog v-model="dialogVisible" :title="\`\$\{dialogProps.title\}\`" width="${width}px" draggable>
+			<div class="view">
+				${type == 1 ? `<el-form ref="${name}FormRef" label-width="100px" label-suffix=" :" :rules="rules" :model="dialogProps!.rowData">
+                ${formGenerator(itemArray)
+            }
+            </el-form>`: ``}
+			</div>
+			<template #footer>
+				<span class="dialog-footer">
+					<el-button @click="dialogVisible = false">取消</el-button>
+					<el-button type="primary" @click="dialogVisible = false">确认</el-button>
+				</span>
+			</template>
+		</el-dialog>
+	</div>
+</template>
+<script setup lang="ts" name="${name}Dialog">
+import { ref, reactive } from "vue";
+import { ElDialog } from "element-plus";
+
+const dialogVisible = ref(false);
+const rules = reactive({
+    ${ruleGenerator(itemArray)}
+});
+
+interface dialogProps {
+	title: string;
+	rowData?: any;
+}
+const dialogProps = ref<dialogProps>({
+	title: ""
+});
+
+// 接收父组件传过来的参数
+const acceptParams = (params: dialogProps): void => {
+	dialogProps.value = params;
+	dialogVisible.value = true;
+};
+defineExpose({
+	acceptParams
+});
+</script>
+<style scoped lang="scss"></style>
+`
+    return template
+}
